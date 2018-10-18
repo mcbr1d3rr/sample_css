e507f2dbfd6aeb842072fc7988c13bf258665276
mcbr1d3rr <randy@rmcbride.com>
2018-10-18 16:50:15 -0600

    remove slide.jpg, update gallery html & js

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,26 @@
 // js/gallery.js
 
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery()
 {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail)
+  {
+    thumbnail.addEventListener("click", function()
+    {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newDataDescription = thumbnail.dataset.description;
+      let newTitle = thumbnail.dataset.title;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newDataDescription);
+      mainImage.setAttribute("title", newTitle);
+
+    });
+  });
 }
